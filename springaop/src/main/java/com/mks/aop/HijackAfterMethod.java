fd7d25999677ec11714b391d20186027a25908f8
kathsi <pkt.siva@gmail.com>
2014-03-11 22:17:57 +0530

    Intiail load AOP

---
+++ springaop/src/main/java/com/mks/aop/HijackAfterMethod.java
@@ -0,0 +1,14 @@
+package com.mks.aop;
+
+import java.lang.reflect.Method;
+
+import org.springframework.aop.AfterReturningAdvice;
+
+public class HijackAfterMethod implements AfterReturningAdvice {
+
+	public void afterReturning(Object arg0, Method arg1, Object[] arg2,
+			Object arg3) throws Throwable {
+		System.out.println("HijackAfterMethod : After method hijacked!");
+	}
+
+}
---
+++ springaop/src/main/java/com/mks/common/App.java
@@ -0,0 +1,30 @@
+package com.mks.common;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.mks.customer.services.CustomerService;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext appContext = new ClassPathXmlApplicationContext(
+				new String[] { "Spring-Customer.xml" });
+
+		CustomerService cust = (CustomerService) appContext.getBean("customerServiceProxy");
+
+		System.out.println("*************************");
+		cust.printName();
+		System.out.println("*************************");
+		cust.printURL();
+		System.out.println("*************************");
+		try {
+			cust.printThrowException();
+		} catch (Exception e) {
+
+		}
+
+	}
+
+}
